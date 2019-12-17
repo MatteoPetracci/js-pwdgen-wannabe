a3cdde04d4ccac8fa658c6ed68f4dffcca177fa2
Matteo <matteop18@gmail.com>
2019-12-17 15:35:27 +0100

    prime prove

--- js/javascript.js
+++ js/javascript.js
@@ -2,12 +2,14 @@
 
 var nomeUtente = prompt("Qual è il tuo nome?");
 // console.log(nomeUtente);
-document.getElementById("nome").innerHTML = nomeUtente;
+// document.getElementById("nome").innerHTML = nomeUtente;
 
 var cognomeUtente = prompt("Qual è il tuo cognome?");
 // console.log(cognomeUtente);
-document.getElementById("cognome").innerHTML = cognomeUtente;
+// document.getElementById("cognome").innerHTML = cognomeUtente;
 
 var colore = prompt("Qual è il tuo colore preferito?");
 // console.log(colore);
-document.getElementById("colore").innerHTML =colore;
+// document.getElementById("colore").innerHTML = colore;
+
+document.getElementById('password').innerHTML = nomeUtente + cognomeUtente + colore;
