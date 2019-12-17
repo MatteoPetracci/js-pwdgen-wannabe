2c917d19c30cac1129d95a2f07c174a6905356b3
Matteo <matteop18@gmail.com>
2019-12-17 15:31:41 +0100

    primo commit

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+
+
+var nomeUtente = prompt("Qual è il tuo nome?");
+// console.log(nomeUtente);
+document.getElementById("nome").innerHTML = nomeUtente;
+
+var cognomeUtente = prompt("Qual è il tuo cognome?");
+// console.log(cognomeUtente);
+document.getElementById("cognome").innerHTML = cognomeUtente;
+
+var colore = prompt("Qual è il tuo colore preferito?");
+// console.log(colore);
+document.getElementById("colore").innerHTML =colore;
